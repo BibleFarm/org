effedb7c11d3e88cd6d9dbee8a63196df584bfd4
BibleFarm <smuglovsky@gmail.com>
2019-01-29 06:49:58 -0300

    .

--- app.js
+++ app.js
@@ -1,58 +1,217 @@
-$(document).ready(function() {
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
-    auth: {
-      params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
-    }
-  });
+/**
+ * FirebaseUI initialization to be used in a Single Page application context.
+ */
 
-  $('.btn-login').click(function(e) {
-    e.preventDefault();
-    lock.show();
-  });
-
-  $('.btn-logout').click(function(e) {
-    e.preventDefault();
-    logout();
-  })
-
-  lock.on("authenticated", function(authResult) {
-    lock.getProfile(authResult.idToken, function(error, profile) {
-      if (error) {
-        // Handle error
-        return;
+/**
+ * @return {!Object} The FirebaseUI config.
+ */
+function getUiConfig() {
+  return {
+    'callbacks': {
+      // Called when the user has been successfully signed in.
+      'signInSuccess': function(user, credential, redirectUrl) {
+        handleSignedInUser(user);
+        // Do not redirect.
+        return false;
       }
-      localStorage.setItem('id_token', authResult.idToken);
-      // Display user information
-      show_profile_info(profile);
-    });
-  });
+    },
+    // Opens IDP Providers sign-in flow in a popup.
+    'signInFlow': 'popup',
+    'signInOptions': [
+      // TODO(developer): Remove the providers you don't need for your app.
+      {
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        scopes: ['https://www.googleapis.com/auth/plus.login']
+      },
+      {
+        provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        scopes :[
+          'public_profile',
+          'email',
+          'user_likes',
+          'user_friends'
+        ]
+      },
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // Whether the display name should be displayed in Sign Up page.
+        requireDisplayName: true
+      }//,
+//      {
+//        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+//        recaptchaParameters: {
+//          size: getRecaptchaMode()
+//        }
+//      }
+    ],
+    // Terms of service url.
+    'tosUrl': 'https://www.google.com'
+  };
+}
 
-  //retrieve the profile:
-  var retrieve_profile = function() {
-    var id_token = localStorage.getItem('id_token');
-    if (id_token) {
-      lock.getProfile(id_token, function (err, profile) {
-        if (err) {
-          return alert('There was an error getting the profile: ' + err.message);
-        }
-        // Display user information
-        show_profile_info(profile);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+
+/**
+ * @return {string} The URL of the FirebaseUI standalone widget.
+ */
+function getWidgetUrl() {
+  return 'widget.html#recaptcha=' + getRecaptchaMode();
+}
+
+
+/**
+ * Redirects to the FirebaseUI widget.
+ */
+var signInWithRedirect = function() {
+  window.location.assign(getWidgetUrl());
+};
+
+
+/**
+ * Open a popup with the FirebaseUI widget.
+ */
+var signInWithPopup = function() {
+  window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
+};
+
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'block';
+  document.getElementById('user-signed-out').style.display = 'none';
+  document.getElementById('name').textContent = user.displayName;
+  document.getElementById('email').textContent = user.email;
+//  document.getElementById('phone').textContent = user.phoneNumber;
+  if (user.photoURL){
+    document.getElementById('photo').src = user.photoURL;
+    document.getElementById('photo').style.display = 'block';
+  } else {
+    document.getElementById('photo').style.display = 'none';
+  }
+
+
+  // --------------------------------------------------------------------
+      $("#continue_as_a_visitor_admin").hide("slow", "linear");
+      $("#admin").show("slow", "linear");
+  // --------------------------------------------------------------------
+
+
+
+
+};
+
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  ui.start('#firebaseui-container', getUiConfig());
+
+
+
+
+
+    // --------------------------------------------------------------------
+      $("#continue_as_a_visitor").show("slow", "linear");
+      $("#admin").hide("slow", "linear");
+  // --------------------------------------------------------------------
+
+
+
+
+
+
+
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Deletes the user's account.
+ */
+var deleteAccount = function() {
+  firebase.auth().currentUser.delete().catch(function(error) {
+    if (error.code == 'auth/requires-recent-login') {
+      // The user's credential is too old. She needs to sign in again.
+      firebase.auth().signOut().then(function() {
+        // The timeout allows the message to be displayed after the UI has
+        // changed to the signed out state.
+        setTimeout(function() {
+          alert('Please sign in again to delete your account.');
+        }, 1);
       });
     }
-  };
+  });
+};
 
-  var show_profile_info = function(profile) {
-     $('.nickname').text(profile.nickname);
-     $('.btn-login').hide();
-     $('.avatar').attr('src', profile.picture).show();
-     $('.btn-logout').show();
-  };
 
-  var logout = function() {
-    localStorage.removeItem('id_token');
-    window.location.href = "/";
-  };
+/**
+ * Handles when the user changes the reCAPTCHA config.
+ */
+function handleRecaptchaConfigChange() {
+  var newRecaptchaValue = document.querySelector(
+      'input[name="recaptcha"]:checked').value;
+  location.replace(location.pathname + '#recaptcha=' + newRecaptchaValue);
 
-  retrieve_profile();
-});
+  // Reset the inline widget so the config changes are reflected.
+  ui.reset();
+  ui.start('#firebaseui-container', getUiConfig());
+}
+
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+  document.getElementById('sign-in-with-redirect').addEventListener(
+      'click', signInWithRedirect);
+  document.getElementById('sign-in-with-popup').addEventListener(
+      'click', signInWithPopup);
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+  document.getElementById('delete-account').addEventListener(
+      'click', function() {
+        deleteAccount();
+      });
+
+  document.getElementById('recaptcha-normal').addEventListener(
+      'change', handleRecaptchaConfigChange);
+  document.getElementById('recaptcha-invisible').addEventListener(
+      'change', handleRecaptchaConfigChange);
+  // Check the selected reCAPTCHA mode.
+  document.querySelector(
+      'input[name="recaptcha"][value="' + getRecaptchaMode() + '"]')
+      .checked = true;
+};
+
+window.addEventListener('load', initApp);
