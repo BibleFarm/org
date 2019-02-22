866dc7672042d72122482a10c756c9961501d92e
BibleFarm <smuglovsky@gmail.com>
2019-02-22 18:23:09 -0300

    .

--- app/056.js
+++ app/056.js
@@ -241,6 +241,8 @@ $(document).ready(function() {
   $("#to_search_reg_eng").one('click', function(e) {
     // user want to search
     console.log("searching feature is being requested");
+    $(".we_are_loading_the_entire_bible").show("slow");
+
 
 
 
@@ -282,6 +284,9 @@ $(document).ready(function() {
     "scrollX": false,
     "scrollY": true,
     "fnInitComplete": function() {
+      console.log( 'DataTables has finished its initialisation.' );
+      $(".we_are_loading_the_entire_bible").hide("slow");
+
       $('.dataTables_scrollBody').perfectScrollbar({
         wheelPropagation: true,
         scrollXMarginOffset: 2
@@ -420,6 +425,9 @@ $(document).ready(function() {
     "scrollX": false,
     "scrollY": true,
     "fnInitComplete": function() {
+      console.log( 'DataTables has finished its initialisation.' );
+      $(".we_are_loading_the_entire_bible").hide("slow");
+
       $('.dataTables_scrollBody').perfectScrollbar({
         wheelPropagation: true,
         scrollXMarginOffset: 2
@@ -8624,5 +8632,64 @@ $(".wrap_modal_still_working_on_this").hide();
   });
   });
   //////////////////////////////////////////////////
-  ////// BEGIN youtube only if requested ////
+  ////// END youtube only if requested ////
+  //////////////////////////////////////////////////
+
+
+  //////////////////////////////////////////////////
+  ////// BEGIN matrix loading entire Bible NN to change to hebrew ////
+  //////////////////////////////////////////////////
+  'use strict';
+
+  const COLUMNS    = 100;
+  const CHARACTERS = 10;
+
+  function getCharCode() {
+    let code = Math.floor(Math.random() * 94 + 33);
+    return (code === 64) ? 47 : code;
+  }
+
+  function getCharCodes() {
+    return Array.from(
+      new Array(CHARACTERS), () => {
+        return String.fromCharCode(getCharCode());
+      }
+    );
+  }
+
+  function createCharCodes(drop) {
+    let codes = getCharCodes(),
+        size  = Math.floor(Math.random() * 13);
+
+    if (size < 10) size = 25;
+    else if (size < 12) size = 12;
+    else size = 60;
+
+    setCharCodes(drop, codes, size);
+    return size;
+  }
+
+  function setCharCodes(column, codes, size) {
+    codes.forEach((code, index) => {
+      let char = document.createElement('span');
+      char.className = `char-${index} code-size-${size}`;
+      char.textContent = code;
+
+      column.appendChild(char);
+    });
+  }
+
+  function createRain(container) {
+    for (let i = 0; i < COLUMNS; i++) {
+      let column = document.createElement('p'),
+          size   = createCharCodes(column);
+
+      column.className = `code-column column-size-${size} code-${i}`;
+      container.appendChild(column);
+    }
+  }
+
+  createRain(document.getElementById('matrix-code'));
+  //////////////////////////////////////////////////
+  ////// END matrix loading entire Bible NN to change to hebrew ////
   //////////////////////////////////////////////////
