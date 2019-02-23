52b647e594c5c2d3d69695a92608c4a13742d494
BibleFarm <smuglovsky@gmail.com>
2019-02-22 21:00:55 -0300

    .

--- app/056.js
+++ app/056.js
@@ -8358,10 +8358,14 @@ if($('#admin').css('display') !== 'none')
   var welcome_user_with_name = $("body").find("#name").text();
   $("#logged_in_user_is_a_part").text("I'm part of biblefarm.org");
   $("#edited_app_name").text(welcome_user_with_name);
-  $(".app_wrap").find("p.msg1").text(welcome_user_with_name).show("slow", "linear");
+  setTimeout(function() {
+  $(".app_wrap").find("p.msg1").text(welcome_user_with_name).fadeOut("5000").fadeIn("7000");
+}, 7000);
 } else {
   $("#logged_in_user_is_a_part").text("be a part of biblefarm.org");
-  $(".app_wrap").find("p.msg1").text("Thanks for visiting").show("slow", "linear");
+  setTimeout(function() {
+  $(".app_wrap").find("p.msg1").text("thanks for visiting").fadeOut("5000").fadeIn("7000");
+}, 7000);
 }
 
   }; // end mutation observer
