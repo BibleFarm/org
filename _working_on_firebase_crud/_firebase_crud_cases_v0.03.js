6e4b42a0752d9dd926bf226c7e18d2feb1d2ecb8
BibleFarm <smuglovsky@gmail.com>
2019-04-18 10:49:36 -0300

    .

--- _working_on_firebase_crud/_firebase_crud_cases_v0.03.js
+++ _working_on_firebase_crud/_firebase_crud_cases_v0.03.js
@@ -381,7 +381,7 @@ table_case_1_a = $("#table_case_1_a").DataTable({
     "scrollX": false,
     "scrollY": "calc(100vh - 262px)",
     language: {
-      url: '../lang/English.json'
+      url: '../lang/Escuela_Jose.json'
     },
         responsive: {
             details: {
@@ -930,7 +930,7 @@ table_case_1_a = $("#table_case_1_a").DataTable({
     "scrollX": false,
     "scrollY": "calc(100vh - 262px)",
     language: {
-      url: '../lang/English.json'
+      url: '../lang/Escuela_Jose.json'
     },
         responsive: {
             details: {
@@ -1419,7 +1419,7 @@ table_case_1_a = $("#table_case_1_a").DataTable({
     "scrollX": false,
     "scrollY": "calc(100vh - 262px)",
     language: {
-      url: '../lang/English.json'
+      url: '../lang/Escuela_Jose.json'
     },
         responsive: {
             details: {
@@ -1944,7 +1944,7 @@ table_case_1_a = $("#table_case_1_a").DataTable({
     "scrollX": false,
     "scrollY": "calc(100vh - 262px)",
     language: {
-      url: '../lang/English.json'
+      url: '../lang/Escuela_Jose.json'
     },
         responsive: {
             details: {
