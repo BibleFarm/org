4b89bdc2b18c03623e8ad15e46d763798aa97c68
BibleFarm <smuglovsky@gmail.com>
2019-04-26 13:53:30 -0300

    .

--- app/en/posts/posts.js
+++ app/en/posts/posts.js
@@ -123,4 +123,112 @@ setTimeout(function(){
 });
   // END done_with_ready_post_close_tab
 
+
+
+  ////////////////////////////////////////////////////
+  // BEGIN PureChat hack
+  ////////////////////////////////////////////////////
+
+  $(document).ready(function() {
+    // initial hide
+    var stopWaitingForPurechat = setInterval(function() {
+      if (
+        $("body").find('.purechat-widget-title-link:contains("Contact us")')
+          .length > 0
+      ) {
+        console.log("found PureChat");
+        // hide it
+        $("#PureChatWidget").attr("style", "z-index: -1 !important");
+        clearInterval(stopWaitingForPurechat);
+      }
+    }, 1000);
+    ////////////////////
+    // show when needed
+    $("#show_pure_chat, .show_pure_chat").on("click", function() {
+      // show it
+      $("#PureChatWidget").attr("style", "z-index: 2147483646 !important");
+      // hack colors
+      // header
+      $("#PureChatWidget")
+        .find(".purechat-widget-header")
+        .attr("style", "background-color: #0c1400 !important");
+      // content-wrapper
+      $("#PureChatWidget")
+        .find(".purechat-content-wrapper")
+        .attr("style", "background-color: #172800 !important");
+      // purechat-card
+      $("#PureChatWidget")
+        .find(".purechat-card")
+        .attr("style", "background-color: #203700 !important");
+      // purechat-btn
+      $("#PureChatWidget")
+        .find(".purechat-btn")
+        .attr("style", "color: #d5ad6d !important");
+      // purechat-widget-title-link
+      $("#PureChatWidget")
+        .find(".purechat-widget-title-link")
+        .attr(
+          "style",
+          "font-size: 20px !important;padding-left: 8px !important;font-family: PT_Sans-Narrow-Web-Regular !important;color: #d5ad6d !important;text-shadow: 2px 2px 1px black !important"
+        );
+      // keep widget expanded
+      $("#PureChatWidget").removeClass("purechat-widget-super-collapsed");
+      $("#PureChatWidget").addClass("purechat-widget-collapsed");
+      console.log("PureChat will keep widget expanded");
+
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 1000);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 1500);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 2000);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 2500);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 3000);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 3500);
+      setTimeout(function() {
+        $("body")
+          .find("#PureChatWidget.purechat.purechat-widget-collapsed")
+          .attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 4000);
+    });
+
+    // hide when click outside
+    $(window).click(function() {
+      // hide it
+      $("#PureChatWidget").attr("style", "z-index: -1 !important");
+    });
+    // stopPropagation when clicking to show the widget and inside the widget itself
+    $("#show_pure_chat, #PureChatWidget").click(function(event) {
+      event.stopPropagation();
+      console.log("PureChat hide when click outside event.stopPropagation");
+    });
+  });
+
+  ////////////////////////////////////////////////////
+  // END PureChat hack
+  ////////////////////////////////////////////////////
+
+
+
 });
