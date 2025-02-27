6ebfbdbe2da864fb11123e28bffdc3c3e1b00158
BibleFarm <smuglovsky@gmail.com>
2025-02-27 13:01:13 -0300

    .

--- website_js/index_cloning_biblecups_into_new_biblefarm_website.js
+++ website_js/index_cloning_biblecups_into_new_biblefarm_website.js
@@ -395,6 +395,66 @@ if ( !$("#591").length) {
 // END smartResizeOrScroll
 /////////////////////////////////////
 
+/////////////////////////////////////
+// BEG lazyloadImages
+/////////////////////////////////////
+var lazyloadImages;
+
+if ("IntersectionObserver" in window) {
+  lazyloadImages = document.querySelectorAll(".lazy");
+  var imageObserver = new IntersectionObserver(function(entries, observer) {
+    console.log(observer);
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        var image = entry.target;
+        image.src = image.dataset.src;
+        image.classList.remove("lazy");
+        imageObserver.unobserve(image);
+      }
+    });
+  }, {
+    root: document.querySelector("#container"),
+    rootMargin: "0px 0px 500px 0px"
+  });
+
+  lazyloadImages.forEach(function(image) {
+    imageObserver.observe(image);
+  });
+} else {
+  var lazyloadThrottleTimeout;
+  lazyloadImages = $(".lazy");
+
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = $(window).scrollTop();
+        lazyloadImages.each(function() {
+            var el = $(this);
+            if(el.offset().top < window.innerHeight + scrollTop + 500) {
+              var url = el.attr("data-src");
+              el.attr("src", url);
+              el.removeClass("lazy");
+              lazyloadImages = $(".lazy");
+            }
+        });
+        if(lazyloadImages.length == 0) {
+          $(document).off("scroll");
+          $(window).off("resize");
+        }
+    }, 20);
+  }
+
+  $(document).on("scroll", lazyload);
+  $(window).on("resize", lazyload);
+}
+
+/////////////////////////////////////
+// END lazyloadImages
+/////////////////////////////////////
+
 
 /* END Document Ready ****** */
 });
