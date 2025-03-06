f687b73e9093911830686b3e42af2b0a62461ce5
BibleFarm <smuglovsky@gmail.com>
2025-03-06 14:33:03 -0300

    Update index_cloning_biblecups_into_new_biblefarm_website.js

--- website_js/index_cloning_biblecups_into_new_biblefarm_website.js
+++ website_js/index_cloning_biblecups_into_new_biblefarm_website.js
@@ -106,7 +106,7 @@ if ( !$("#591").length) {
 
 //  $('.gofundme_flex-container').append($('<div>').load('https://www.biblefarm.org/data_files/ALL_visits_flex_items.html #' + '591'));
 // add the visit divs
-  $('.gofundme_flex-container').load('https://www.biblefarm.org/data_files/ALL_visits_flex_items.html');
+  $('.gofundme_flex-container').load('https://www.biblefarm.org/data_files/ALL_visits_flex_items.html .wrap_featured_item').slice( 0, 2 );
 
 setTimeout(function() {
 ////////////////////////////////////////////////////
