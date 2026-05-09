ebb448d5d1090a55351d029df6be7d556c9ba0d2
BibleFarm <smuglovsky@gmail.com>
2026-05-08 22:38:14 -0300

    .

--- website_js/index.js
+++ website_js/index.js
@@ -253,7 +253,7 @@ $(document).ready(function() {
   // BEG append visit counter and today's date to showcase-content
   /////////////////////////////////////
   //setTimeout(function() {
-  $('.showcase-content').append('<h3><div class="timerDiv"><span class="timer count-title count-number" data-to="643" data-speed="10000"></span>643 visits as of today, </span><span id="month">11</span> <span id="day">11</span>, <span id="year">11</span></div></h3>');
+  $('.showcase-content').append('<h3><div class="timerDiv"><span class="timer count-title count-number" data-to="644" data-speed="10000"></span>644 visits as of today, </span><span id="month">11</span> <span id="day">11</span>, <span id="year">11</span></div></h3>');
   //},2000);
   /////////////////////////////////////
   // END append visit counter and today's date to showcase-content
